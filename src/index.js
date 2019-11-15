ca8d697affb2ab2a86a15828a801d80c943c4464
Anorak Tha Great <anorakthagreat@gmail.com>
2019-11-15 00:04:39 -0700

    fix linter warnings/errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { configureStore } from 'redux-starter-kit';
 import App from './App';
-import {configureStore} from 'redux-starter-kit';
 
 import homePageReducer from './pages/HomePage/homePage';
 
 const store = configureStore({
-    reducer : {
-        Home : homePageReducer
-    }
+  reducer: {
+    Home: homePageReducer
+  }
 });
- 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
